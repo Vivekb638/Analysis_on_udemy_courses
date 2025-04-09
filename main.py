324aaba8378b8eabf0beb2f239e6424b554c98c9
Bhoomi64 <149092087+Bhoomi64@users.noreply.github.com>
2025-04-10 02:53:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,42 @@ df.drop_duplicates(inplace=True)
 df['price'] = df['price'].replace('Free', 0).astype(int)
 df['published_timestamp'] = pd.to_datetime(df['published_timestamp'], errors='coerce')
 
-# ========== 1. BASIC INFO ==========
+#EDA
+print("Summary of dataset:\n")
+print(df.info())
+print("Summary Statistics:\n")
+print(df.describe())
+
 print(" Dataset Shape:", df.shape)
 print("\n Column Names:\n", df.columns.tolist())
 print("\n Data Types:\n", df.dtypes)
 print("\n Missing Values:\n", df.isnull().sum())
 print("\n Duplicate Rows:", df.duplicated().sum())
+
+#correlaation
+correlation_matrix = df.corr(numeric_only=True)
+sns.set(style="white", font_scale=1.1)
+plt.figure(figsize=(10, 7))
+sns.heatmap(
+    correlation_matrix,
+    annot=True,
+    linewidths=0.5,
+)
+plt.title("Correlation Heatmap of Numeric Features", fontsize=14, weight='bold')
+plt.tight_layout()
+plt.show()
+
+#outliers
+numeric_cols = df.select_dtypes(include=[np.number]).columns
+z_scores = df[numeric_cols].apply(zscore)
+z_threshold = 3
+outliers_by_column = {}
+
+for col in numeric_cols:
+    outliers_count = (np.abs(z_scores[col]) > z_threshold).sum()
+    if outliers_count > 0:
+        outliers_by_column[col] = outliers_count
+
+print("Outliers detected using Z-score method:")
+for col, count in outliers_by_column.items():
+    print(f"{col}: {count} outliers")
